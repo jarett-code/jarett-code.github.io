3099f5f3116d88a98407c1fca6bd410b7452c33e
Jarett Noade <jarett.noade@dfo-mpo.gc.ca>
2025-01-12 15:03:09 -0400

    Can now see what the players' roles were when the game is ended.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 // End the game. Keeps the players, but wipes their roles and abilities.
 function endGame() {
-    const storedPlayers = JSON.parse(localStorage.getItem('players'));
-    if (storedPlayers) {
-        // Reset all players to their initial state
-        const resetPlayers = storedPlayers.map(player => ({ ...player, role: '', usedAbility: false }));
-        localStorage.setItem('players', JSON.stringify(resetPlayers));        
+    // const storedPlayers = JSON.parse(localStorage.getItem('players'));
+    // if (storedPlayers) {
+    //     // Reset all players to their initial state
+    //     const resetPlayers = storedPlayers.map(player => ({ ...player, role: '', usedAbility: false }));
+    //     localStorage.setItem('players', JSON.stringify(resetPlayers));        
+    // }
+
+    // If the game was started, set a games state for the lobby to show all players' roles
+    if (localStorage.getItem('isGameStarted')) {
+        localStorage.setItem('showPlayerRoles', 'yes');
     }
 
     // Reset extra roles and game state variables
@@ -62,3 +67,7 @@ if (resetButton) {
     });
 }
 
+function useAorAn(word) {
+    // Depending on the first letter of the word, return 'a' or 'an'
+    return ['a', 'e', 'i', 'o', 'u'].includes(word[0].toLowerCase()) ? 'an' : 'a';
+}
